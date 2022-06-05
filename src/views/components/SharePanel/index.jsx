c7c8f92e9c5de2f10a2df2a65ac4313cb91d5066
Maxime Clauss <maxime@softwarefoundry.hk>
2022-06-05 11:28:18 +0800

    Improve email validation

--- src/views/components/SharePanel/index.jsx
+++ src/views/components/SharePanel/index.jsx
@@ -14,8 +14,9 @@ const { HOME } = ROUTE_NAMES;
 
 const throttleTime = 2000;
 const mailchimpUrl = "https://better.us17.list-manage.com/subscribe/post?u=a0240e5eff33764063f2fe143&amp;id=4821260ae2";
-
-const isEmailValid = (email) => !!(email.indexOf("@") > -1);
+// eslint-disable-next-line no-control-regex
+const emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const isEmailValid = (email) => emailPattern.test(email);
 
 const SharePanel = () => {
   return (
