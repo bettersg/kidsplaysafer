2972ee7fb0be2985b2998135187901530e62663c
Maxime Clauss <maxime.clauss@gmail.com>
2022-06-09 21:46:16 +0800

    Change to KPS mailchimp account

--- src/views/components/SharePanel/index.jsx
+++ src/views/components/SharePanel/index.jsx
@@ -13,7 +13,7 @@ import ROUTE_NAMES from "../../../constants/routeNames";
 const { HOME } = ROUTE_NAMES;
 
 const throttleTime = 2000;
-const mailchimpUrl = "https://better.us17.list-manage.com/subscribe/post?u=a0240e5eff33764063f2fe143&amp;id=4821260ae2";
+const mailchimpUrl = "https://better.us10.list-manage.com/subscribe/post?u=5c87c359df2b6af39788321f4&amp;id=f06bc771c8";
 // eslint-disable-next-line no-control-regex
 const emailPattern = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 const isEmailValid = (email) => emailPattern.test(email);
