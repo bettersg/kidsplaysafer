4464cf8041c0fdbc3eed30a80fec74232cc586ac
Maxime Clauss <maxime.clauss@gmail.com>
2022-06-28 00:38:37 +0800

    Remove unused import

--- src/views/components/AvatarPanel/index.jsx
+++ src/views/components/AvatarPanel/index.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
